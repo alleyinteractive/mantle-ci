ef370677e63df7460b9495878db3dc7de16bffa6
Sean Fisher <srtfisher@gmail.com>
2022-12-15 12:44:38 -0500

    Fix the URL for db.php

--- install-wp-tests.sh
+++ install-wp-tests.sh
@@ -106,7 +106,7 @@ install_wp() {
 		tar --strip-components=1 -zxmf $CACHEDIR/wordpress.tar.gz -C $WP_CORE_DIR
 	fi
 
-	download https://raw.github.com/markoheijnen/wp-mysqli/master/db.php $WP_CORE_DIR/wp-content/db.php
+	download https://raw.githubusercontent.com/markoheijnen/wp-mysqli/master/db.php $WP_CORE_DIR/wp-content/db.php
 }
 
 install_config() {
